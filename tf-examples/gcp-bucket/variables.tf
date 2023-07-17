776017fae5cda61c8de901e6b4dbf916f163b8b4
Piyush Patel <piyushpatel2005@gmail.com>
2023-07-16 20:26:45 -0400

    Added missing variable

--- tf-examples/gcp-bucket/variables.tf
+++ tf-examples/gcp-bucket/variables.tf
@@ -8,3 +8,9 @@ variable "region" {
     description = "The Region where to deploy resource"
     default = "us-west1"
 }
+
+variable "zone" {
+  type = string
+  description = "The default zone"
+  default = "us-west1-a"
+}
